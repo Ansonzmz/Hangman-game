34b3178b75e5db7d8de39dc8a30ba82aaf8eb47d
Anson <zmz211@gmail.com>
2020-12-05 00:55:52 -0800

    start js

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+const wordEl = document.getElementById('word');
+const wrongLetterEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
