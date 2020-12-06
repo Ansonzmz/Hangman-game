0c7034a2805db2113a7093caf219a41ca93a7b38
Anson <zmz211@gmail.com>
2020-12-05 16:31:56 -0800

    js

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,28 @@ const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard', 'python', 'canada'];
+
+// randomly pick the word from array
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+
+//console.log(selectedWord);
+
+const correctLetters = [];
+const wrongLetters = [];
+
+//show the hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord
+        .split('')
+        .map(letter => `
+        <span class = "letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        `).join('')}
+    `;
+}
+
+displayWord()
